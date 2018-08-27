7d53d522c477d288b54a42ba90eb3eb5bf581ff7
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2018-08-27 20:43:52 +0700

    sklearn based realisation of balanced_accurecy_score function

---
+++ courses/dl1/my_utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def balanced_accuracy_score(y_true, y_pred):
+    """Compute the balanced accuracy
+    The balanced accuracy in binary and multiclass classification problems to
+    deal with imbalanced datasets. It is defined as the average of recall
+    obtained on each class.
+    Parameters
+    ----------
+    y_true : 1d array-like
+        Ground truth (correct) target values.
+    y_pred : 1d array-like
+        Estimated targets as returned by a classifier.
+    Returns
+    -------
+    balanced_accuracy : float
+    See also
+    --------
+    recall_score, roc_auc_score
+    References
+    ----------
+    .. [1] Brodersen, K.H.; Ong, C.S.; Stephan, K.E.; Buhmann, J.M. (2010).
+           The balanced accuracy and its posterior distribution.
+           Proceedings of the 20th International Conference on Pattern
+           Recognition, 3121-24.
+    .. [2] John. D. Kelleher, Brian Mac Namee, Aoife D'Arcy, (2015).
+           `Fundamentals of Machine Learning for Predictive Data Analytics:
+           Algorithms, Worked Examples, and Case Studies
+           <https://mitpress.mit.edu/books/fundamentals-machine-learning-predictive-data-analytics>`_.
+    Examples
+    --------
+    >>> from my_utils import balanced_accuracy_score
+    >>> y_true = [0, 1, 0, 0, 1, 0]
+    >>> y_pred = [0, 1, 0, 0, 0, 1]
+    >>> balanced_accuracy_score(y_true, y_pred)
+    0.625
+    """
+    C = confusion_matrix(y_true, y_pred)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        per_class = np.diag(C) / C.sum(axis=1)
+    if np.any(np.isnan(per_class)):
+        per_class = per_class[~np.isnan(per_class)]
+    score = np.mean(per_class)
+    return score
+
